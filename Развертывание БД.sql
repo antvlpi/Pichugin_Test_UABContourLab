948d7eb575ab089ef77de77ac13959ec2e773aa0
Anton Pichugin <antvlpi@gmail.com>
2022-01-26 23:16:50 +0700

    /

--- Развертывание БД.sql
+++ Развертывание БД.sql
@@ -18,10 +18,3 @@ CREATE TABLE Employee
 	CONSTRAINT FK_Department_ID FOREIGN KEY (department_id) REFERENCES Department(id),
 	CONSTRAINT FK_Chief_ID FOREIGN KEY (department_id) REFERENCES Employee(id)
 );
-
-SELECT em1.chief_id ID_руководителя, em1.name ФИО, em1.salary ЗП, de.name Отдел FROM Employee AS em1
-INNER JOIN Employee AS em2
-ON em1.chief_id = em2.id
-INNER JOIN Department AS de
-ON em1.department_id = de.id
-WHERE (em1.chief_id = em2.id and em1.salary > em2.salary);
